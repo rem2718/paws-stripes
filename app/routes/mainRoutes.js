64efa1046a8a5cece80c6c1dc2b6bf40c217aac0
reme2.718 <103392516+rem2718@users.noreply.github.com>
2023-04-18 06:39:42 +0300

    api

--- app/routes/mainRoutes.js
+++ app/routes/mainRoutes.js
@@ -1,24 +1,23 @@
 const express = require('express');
+const requests = require('./requestsRoutes');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('/index.html');
 });
 
-router.get('/meet-our-pets', (req, res) => {
-    res.render('meet-our-pets');
-});
-
-router.get('/requests', (req, res) => {
-    res.render('requests');
-});
-
-router.get('/adoption-experiences', (req, res) => {
-    res.render('adoption-experiences');
-});
+router.use('/requests', requests);
 
 router.get('/rescue-form', (req, res) => {
     res.render('rescue-form');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
 });
 
 module.exports = router;
--- app/routes/volunteerRoutes.js
+++ app/routes/volunteerRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const volunteerController = require('../controllers/volunteerController');
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
 router.post('/', volunteerController.createVolunteer);
 router.put('/:id', volunteerController.updateStatus);
