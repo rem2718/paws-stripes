db5c29671a21e1b0f7d5ea574e97e02d8e13e7da
reme2.718 <103392516+rem2718@users.noreply.github.com>
2023-04-20 08:14:08 +0300

    experiences

--- app/routes/experienceRoutes.js
+++ app/routes/experienceRoutes.js
@@ -4,9 +4,9 @@ const controller = require('../controllers/experienceController');
 const router = express.Router({ mergeParams: true });
 
 router.post('/', [auth], controller.createExperience);
-router.post('/:id', [auth], controller.like);
+router.put('/:id', [auth], controller.like);
 router.get('/', controller.getExperiences);
 router.get('/:id', controller.getExperience);
-router.delete('/', [auth], controller.deleteExperience);
+router.delete('/:id', [auth], controller.deleteExperience);
 
 module.exports = router;
