685cfd7a0c3499c59ec8cd9526e76c3843d50a77
yamat0002 <220410035@psu.edu.sa>
2023-04-20 06:10:23 +0300

    idk

--- app/utils/database.js
+++ app/utils/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const debug = require('debug')('app:database')
 mongoose.connect('mongodb+srv://rem:paws2020@paws-and-stripes.ovdm3cw.mongodb.net/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true
